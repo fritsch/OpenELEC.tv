0028fee3d0e29b3585d4bde7605f26ac3877787b
fritsch <fritsch@xbmc.org>
2015-12-02 12:04:00 +0100

    kodi: bump to kodi-16.0-beta3-097a209

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="16.0-beta3-10362ce"
+PKG_VERSION="16.0-beta3-097a209"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
