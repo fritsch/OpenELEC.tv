c38e01b86cfa5f7c7b9215ae574e349ae860615f
fritsch <fritsch@xbmc.org>
2015-12-02 11:59:32 +0100

    xf86-video-intel: bump to da9ad3

--- packages/x11/driver/xf86-video-intel/package.mk
+++ packages/x11/driver/xf86-video-intel/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-video-intel"
-PKG_VERSION="627ef68"
+PKG_VERSION="da9ad3"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
