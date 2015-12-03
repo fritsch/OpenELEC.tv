c43dcf864d5fef7382fc6e5a49bea20057383b6a
fritsch <fritsch@xbmc.org>
2015-12-03 15:30:28 +0100

    Binary Addons: Bump

--- packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.vdr.vnsi"
-PKG_VERSION="5eba869"
+PKG_VERSION="92d452f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
