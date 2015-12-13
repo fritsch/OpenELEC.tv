95aed65abecd95fa12d8709ce31a77674615dd3f
fritsch <peter.fruehberger@gmail.com>
2015-12-13 11:11:41 +0100

    ffmpeg: Package kodi's ffmpeg releases directly

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -17,12 +17,13 @@
 ################################################################################
 
 PKG_NAME="ffmpeg"
-PKG_VERSION="2.8.3"
+PKG_VERSION="2.8.4-Jarvis-beta4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2.1+"
 PKG_SITE="https://ffmpeg.org"
-PKG_URL="https://ffmpeg.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/xbmc/FFmpeg/archive/${PKG_VERSION}.tar.gz"
+PKG_SOURCE_DIR="FFmpeg-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain yasm:host zlib bzip2 libvorbis libressl dcadec"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
@@ -213,4 +214,8 @@ configure_target() {
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/share/ffmpeg/examples
+}
+
+unpack() {
+  tar xf $ROOT/$SOURCES/ffmpeg/$PKG_VERSION.tar.gz -C $ROOT/$BUILD
 }
