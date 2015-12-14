96ae114c08c1932dfa63086d3a8c7fa42aa81458
fritsch <peter.fruehberger@gmail.com>
2015-12-14 21:16:54 +0100

    kodi: bump to 17.0-alpha1-318931e (add temp patch to fix mp3 playback from library)

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="17.0-alpha1-945b9f8"
+PKG_VERSION="17.0-alpha1-318931e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
