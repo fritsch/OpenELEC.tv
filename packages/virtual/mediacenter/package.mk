51838648f92fc28017dddeef02b266635182df54
fritsch <peter.fruehberger@gmail.com>
2016-01-21 08:25:46 +0100

    binary addons: disable pvr.demo pvr.njoy pvr.wmc

--- packages/virtual/mediacenter/package.mk
+++ packages/virtual/mediacenter/package.mk
@@ -44,7 +44,6 @@ if [ "$MEDIACENTER" = "kodi" ]; then
 
 # other packages
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET OpenELEC-settings"
-
   if [ "$KODI_LANGUAGE_ADDONS" = "yes" ]; then
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET kodi-language-addons"
   fi
