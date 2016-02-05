8d8ccf3ad78b780c84fca6aaa84975246731d820
fritsch <peter.fruehberger@gmail.com>
2016-02-05 16:39:17 +0100

    kodi: bump to 17.0-alpha1-8772a2a

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="17.0-alpha1-05945fd"
+PKG_VERSION="17.0-alpha1-8772a2a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
