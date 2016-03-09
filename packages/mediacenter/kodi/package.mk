77880188e57d633250b70eb3696a7fb8b06b32ea
fritsch <peter.fruehberger@gmail.com>
2016-03-09 14:44:37 +0100

    kodi: bump to 6638832

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="17.0-alpha1-70a7dfa"
+PKG_VERSION="17.0-alpha1-6638832"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
