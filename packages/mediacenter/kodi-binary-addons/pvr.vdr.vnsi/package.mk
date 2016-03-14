bb2ae243356aa26e660dd29506641c55377203d3
fritsch <peter.fruehberger@gmail.com>
2016-03-14 23:20:15 +0100

    Bump binary addons

--- packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.vdr.vnsi"
-PKG_VERSION="2431fa4"
+PKG_VERSION="902a3cd"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
