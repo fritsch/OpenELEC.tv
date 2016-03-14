823b925576b5b533d452a6a989313e30efe17e45
fritsch <peter.fruehberger@gmail.com>
2016-03-14 19:58:50 +0100

    kodi: bump to 17.0-alpha1-afbd88a

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="17.0-alpha1-baacdd2"
+PKG_VERSION="17.0-alpha1-afbd88a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
