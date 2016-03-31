97528606d9baab499924fcaf9fd95a524b1416d5
fritsch <peter.fruehberger@gmail.com>
2016-03-31 21:53:49 +0200

    kodi: bump to 17.0-alpha1-4496fe2

--- packages/mediacenter/kodi-theme-Estuary/package.mk
+++ packages/mediacenter/kodi-theme-Estuary/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Estuary"
-PKG_VERSION="17.0-alpha1-188dda4"
+PKG_VERSION="17.0-alpha1-4496fe2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
